05ecae9c39f9fc117764d5786b8bdfc4eaeb41f6
Razor <talhagumuskayaa@gmail.com>
2026-04-25 17:10:34 +0300

    Create 7-boolean.py

---
+++ src/7-boolean.py
@@ -0,0 +1,20 @@
+# ==========================================
+#                   BOOLEAN
+# ==========================================
+
+myBoolean = True # Boolean Ya True Olabilir
+print(myBoolean)
+
+myBoolean = False # Ya Da False, Yalnızca İki Değer Var.
+print(myBoolean)
+
+# Koşul Kontrollerinde Yardımcı Olabilecek Bir Komut.
+
+print(5 < 3) # False Değeri Döndü, Kontrol Sonuçları Boolean Değerinde Döner.
+
+userAge = input("Yaşınızı Girin: ")
+print(int(userAge) > 18) # 'userAge' Değişkenini Karşılaştırmaya Sokmadan Önce Integer'a Çevirmemiz Lazım Yoksa Error Verir.
+
+
+
+
